e3fae40029e150f33122cfb1369796e2278aab29
Marcel P <marcel.pludra@t-online.de>
2022-02-02 21:25:12 +0000

    Script

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World2222"}
+    return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
